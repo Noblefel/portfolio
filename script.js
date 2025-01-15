bd17b541310a070096467e07c047fdf12229e1ce
Noblefel <grahamlotford9@gmail.com>
2025-01-15 14:19:31 +0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const toggles = document.querySelectorAll(".toggle");
 const body = document.getElementsByTagName("body")[0];
 
-toggles.forEach((e) => {
-  e.addEventListener("click", toggleTheme);
-});
+toggles.forEach((e) => e.addEventListener("click", toggleTheme));
 
 function toggleTheme() {
   if (body.classList.contains("light")) {
@@ -38,21 +36,44 @@ routes.forEach((e) => {
 
 function setPage(id) {
   pages.forEach((e) => {
-    if (e.id != id) {
-      e.style.display = "none";
-      return;
-    }
-
-    e.style.display = "block";
+    e.style.display = e.id == id ? "block" : "none";
   });
 
   routes.forEach((e) => {
-    if (e.getAttribute("data-nav") != id) {
-      e.classList.remove("active");
-      return;
-    }
-    e.classList.add("active");
+    e.getAttribute("data-nav") == id
+      ? e.classList.add("active")
+      : e.classList.remove("active");
   });
 
   window.scrollTo(0, 0);
+}
+
+const filters = document.querySelectorAll("#filter .item");
+const projects = document.querySelectorAll("#projects article");
+
+filters.forEach((e) => e.addEventListener("click", setFilter));
+
+function setFilter(e) {
+  const filter = e.target.innerText.toLowerCase();
+
+  filters.forEach((e) => e.classList.remove("active"));
+  e.target.classList.add("active");
+
+  if (filter == "semua") {
+    projects.forEach((e) => (e.style.display = "block"));
+    return;
+  }
+
+  projects.forEach((e) => {
+    const tags = e.querySelectorAll(".tags span");
+
+    for (const tag of tags) {
+      if (tag.innerText.toLowerCase() == filter) {
+        e.style.display = "block";
+        return;
+      }
+    }
+
+    e.style.display = "none";
+  });
 }
